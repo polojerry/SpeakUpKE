58509a7aca49805f3a29701a7baa74e4bd3a7404
polojerry <polojerry4@gmail.com>
2019-11-12 13:34:20 +0300

    Corrected import errors

--- app/src/main/java/com/polotechnologies/speakupke/MainActivity.java
+++ app/src/main/java/com/polotechnologies/speakupke/MainActivity.java
@@ -1,11 +1,14 @@
 package com.polotechnologies.speakupke;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayout;
+
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.gridlayout.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
